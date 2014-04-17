d4d4b85510b771cd344abd6305e860ce125586c2
Scott Feeney <scott@oceanbase.org>
2014-04-16 23:43:03 -0400

    Add lookup benchmarks & make others consistent
    This introduces two benchmarks for lookup, one sequential, one in
    random order. Also makes all benchmarks consistent in repeating the
    operation under test 1000 times.

--- benchmark/index.js
+++ benchmark/index.js
@@ -3,35 +3,66 @@ var PV;
 var changeStuffSuite = function(impl) {
   return function() {
     var vec10k;
-    var length = 10000;
+    const length = 10000;
+    const repCount = 1000; // should be less than length
 
     before(function() {
       PV = impl;
       vec10k = new PV();
       for (var i = 0; i < length; i++) {
-        vec10k = vec10k.push(Math.floor(Math.random()*100));
+        vec10k = vec10k.push(Math.floor(Math.random()*length));
       }
     });
 
-    bench('change the first element 1000 times', function() {
+    bench('set first element repeatedly', function() {
       var vec = vec10k;
-      for (var i = 0; i < 1000; i++) {
+      for (var i = 0; i < repCount; i++) {
         vec = vec.set(0, i&1);
       }
     });
 
-    bench('change the last element 1000 times', function() {
+    bench('set last element repeatedly', function() {
       var vec = vec10k;
-      for (var i = 0; i < 1000; i++) {
+      for (var i = 0; i < repCount; i++) {
         vec = vec.set(length-1, i&1);
       }
     });
 
-    bench('add 500 new elements to the end', function() {
+    bench('push elements', function() {
       var vec = vec10k;
-      for (var i = 0; i < 500; i++) {
+      for (var i = 0; i < repCount; i++) {
         vec = vec.push(i&1);
       }
+    });
+
+    bench('get random elements', function() {
+      // This test exploits that each element's value in vec10k is also
+      // a valid index.
+      var index = length / 2;
+      for (var i = 0; i < repCount; i++) {
+        var newIndex = vec10k.get(index);
+        if (newIndex === index) newIndex++;
+        index = newIndex;
+      }
+      // Following is just to prevent above loop from getting optimized away.
+      if (index < 0) throw new Error('this will never happen');
+    });
+
+    bench('get sequential elements', function() {
+      // Flag is to prevent the whole loop getting optimized away.
+      var flag = true;
+
+      // Start at a random location
+      var start = Math.floor(Math.random() * (length - repCount));
+      var end = start + repCount;
+
+      for (var i = start; i <= end; i++) {
+        if (vec10k.get(i) < 0) {
+          flag = false;
+        }
+      }
+
+      if (!flag) throw new Error('this will never happen');
     });
   };
 };
